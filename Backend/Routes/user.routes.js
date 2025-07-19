c795e6f8cb2d0b716303dd30cf18b5db2b21a7f4
rijans <makwanarijans@gmail.com>
2025-07-19 12:24:48 +0530

    User-Authentication

--- Backend/Routes/user.routes.js
+++ Backend/Routes/user.routes.js
@@ -3,18 +3,39 @@ const router = express.Router();
 
 const userController = require("../controllers/user.controller");
 const { body } = require("express-validator");
+const authmiddleware=require("../middlewares/auth.middleware")
 
-
-router.post("/register",[
-  body("email").isEmail().withMessage("Please enter a valid email address"),
-  body("fullname.firstname")
-    .isLength({ min: 3 })
-    .withMessage("First name must be at least 3 characters long"),
-  body("password")
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters long"),
-],
-userController.registerUser
+// Registering User
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email address"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.registerUser
 );
+
+// Logging User
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("password should be minimum 6 letters"),
+  ],
+  userController.loginUser
+);
+
+//Getting Profile
+router.get('/profile',authmiddleware.authUser,userController.getUserProfile)
+
+//Logout User
+router.get('/logout',authmiddleware.authUser,userController.logoutUser)
 
 module.exports = router;
