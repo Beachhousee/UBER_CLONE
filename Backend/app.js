ad709c6a10850b5b81583d82fdd3d70e650bb2fd
rijans <makwanarijans@gmail.com>
2025-07-19 13:25:51 +0530

    Captain Registeration

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,13 @@ const dotenv = require('dotenv')
 dotenv.config();
 const express=require('express')
 const cors=require('cors')
-const userRoutes=require('./Routes/user.routes')
+const userRoutes=require('./routes/user.routes');
+const captainRoutes=require('./routes/captain.routes');
 const app=express();
 const cookieParser = require('cookie-parser');
-const connectToDb = require('./db/db');
+const connectToDb = require('./middlewares/db/db');
 connectToDb();
+
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +19,7 @@ app.get('/', (req,res)=>{
   res.send("hello world")
 })
 
-app.use ('/users',userRoutes)
+app.use('/users', userRoutes);
+app.use('/captains',captainRoutes);
 
 module.exports = app
